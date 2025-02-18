cd3efc26bc727511a25855947ab0e0a0e150bac9
arman07k <hossenarmanshakuat@gmail.com>
2025-02-18 17:47:56 +0600

    Update problem_02.js

--- problem_02.js
+++ problem_02.js
@@ -11,17 +11,17 @@ function validContact(contact){
 }
 
 
-const val1 = validContact("01819234567");
-console.log(val1);
-const val2 = validContact("0181923 4567");
-console.log(val2);
-const val3 = validContact("018192345679");
-console.log(val3);
-const val4 = validContact(["01987654321"]);
-console.log(val4);
-const val5 = validContact("01345678900");
-console.log(val5);
-const val6 = validContact("02145678900");
-console.log(val6);
-const val7 = validContact(true);
-console.log(val7);
+// const val1 = validContact("01819234567");
+// console.log(val1);
+// const val2 = validContact("0181923 4567");
+// console.log(val2);
+// const val3 = validContact("018192345679");
+// console.log(val3);
+// const val4 = validContact(["01987654321"]);
+// console.log(val4);
+// const val5 = validContact("01345678900");
+// console.log(val5);
+// const val6 = validContact("02145678900");
+// console.log(val6);
+// const val7 = validContact(true);
+// console.log(val7);
