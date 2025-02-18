e709d5a76ea438213edf3fd88dcf715d22a05a9e
arman07k <hossenarmanshakuat@gmail.com>
2025-02-18 17:48:58 +0600

    Update problem_05.js

--- problem_05.js
+++ problem_05.js
@@ -17,18 +17,18 @@ function calculateSleepTime(times) {
     return { hour, minute, second };
 }
 
-const arr1 = [1000, 499, 519, 300];
-console.log(calculateSleepTime(arr1));
-const arr2 = [1000, 2000, 725];
-console.log(calculateSleepTime(arr2));
-const arr3 = [100, 3800];
-console.log(calculateSleepTime(arr3));
-const arr4 = [];
-console.log(calculateSleepTime(arr4));
-const arr5 = [5600];
-console.log(calculateSleepTime(arr5));
-const arr6 = [100, 3800, "90"];
-console.log(calculateSleepTime(arr6));
+// const arr1 = [1000, 499, 519, 300];
+// console.log(calculateSleepTime(arr1));
+// const arr2 = [1000, 2000, 725];
+// console.log(calculateSleepTime(arr2));
+// const arr3 = [100, 3800];
+// console.log(calculateSleepTime(arr3));
+// const arr4 = [];
+// console.log(calculateSleepTime(arr4));
+// const arr5 = [5600];
+// console.log(calculateSleepTime(arr5));
+// const arr6 = [100, 3800, "90"];
+// console.log(calculateSleepTime(arr6));
 
 
 
